f58fec87d02f28a46345f6405d46c00e3d9148c2
Mirko Vitale <mirko.vitale.1997@outlook.com>
2024-07-31 10:13:53 +0200

    fix code

--- React/live-30-07-2024/my-app/src/Tris.jsx
+++ React/live-30-07-2024/my-app/src/Tris.jsx
@@ -10,87 +10,39 @@ export function Tris() {
   function handleClick(row, col) {
     if (matrix[row][col] == 0) {
       matrix[row][col] = symbol;
-      for (let i = 0; i < matrix.length; i++) {
-        if (matrix[i].find((s) => s != symbol) === undefined) {
-          alert(
-            `il simbolo che ha vinto è ${
-              symbol == 1 ? "croce" : symbol == 2 ? "cerchio" : ""
-            }`
-          );
-          setMatrix([
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-          ]);
-          setSymbol(1);
-          return;
-        }
-      }
-
+      const arrays = []
+      const diagonal1 = [];
+      const diagonal2 = [];
       for (let i = 0; i < matrix.length; i++) {
         const colonna = [];
         for (let j = 0; j < matrix.length; j++) {
           colonna.push(matrix[j][i]);
         }
-        if (colonna.find((s) => s != symbol) === undefined) {
-          alert(
-            `il simbolo che ha vinto è ${
-              symbol == 1 ? "croce" : symbol == 2 ? "cerchio" : ""
-            }`
-          );
-          setMatrix([
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-          ]);
-          setSymbol(1);
+        diagonal1.push(matrix[i][i]);
+        diagonal2.push(matrix[i][matrix.length - 1 - i]);
+        arrays.push(matrix[i], colonna)
+      }
+      arrays.push(diagonal1, diagonal2)
+      for(let i = 0; i < arrays.length; i++){
+        if (arrays[i].find((s) => s != symbol) === undefined) {
+          vittoria();
           return;
         }
       }
 
-      const diagonal1 = [];
-      const diagonal2 = [];
-
-      for (let i = 0; i < matrix.length; i++) {
-        diagonal1.push(matrix[i][i]);
-        diagonal2.push(matrix[i][matrix.length - 1 - i]);
-      }
-      if (
-        diagonal1.find((s) => s != symbol) === undefined ||
-        diagonal2.find((s) => s != symbol) === undefined
-      ) {
-        alert(
-          `il simbolo che ha vinto è ${
-            symbol == 1 ? "croce" : symbol == 2 ? "cerchio" : ""
-          }`
-        );
-        setMatrix([
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-        ]);
-        setSymbol(1);
-        return;
-      }
-      let pareggio = true;
       
+      let even = true;
+
       for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
           if (matrix[j][i] === 0) {
-            pareggio = false
-            break
+            even = false;
+            break;
           }
         }
-      } if(pareggio){
-        alert(
-          `pareggio`
-        );
-        setMatrix([
-          [0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0],
-        ]);
-        setSymbol(1);
+      }
+      if (even) {
+        pareggio()
         return;
       }
 
@@ -101,6 +53,29 @@ export function Tris() {
       });
       setSymbol(symbol === 1 ? 2 : 1);
     }
+  }
+  function vittoria() {
+    alert(
+      `il simbolo che ha vinto è ${
+        symbol == 1 ? "croce" : symbol == 2 ? "cerchio" : ""
+      }`
+    );
+    setMatrix([
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]);
+    setSymbol(1);
+  }
+
+  function pareggio() {
+    alert(`pareggio`);
+    setMatrix([
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]);
+    setSymbol(1);
   }
 
   return (
