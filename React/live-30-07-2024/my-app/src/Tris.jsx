42b1de319a08082ce1e7be45581b4f9fce7b9323
massimo altamura <maltamura494@gmail.com>
2024-08-01 09:47:09 +0200

    eserecizio live 01/08

--- React/live-30-07-2024/my-app/src/Tris.jsx
+++ React/live-30-07-2024/my-app/src/Tris.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function Tris() {
   const [matrix, setMatrix] = useState([
@@ -7,45 +7,54 @@ export function Tris() {
     [0, 0, 0],
   ]);
   const [symbol, setSymbol] = useState(1);
-  function handleClick(row, col) {
-    if (matrix[row][col] == 0) {
-      matrix[row][col] = symbol;
+
+  useEffect(()=>{
+    console.log("controllo")
+    const simboli = [1,2]
+    for(let k = 0; k<simboli.length;k++){
       const arrays = []
-      const diagonal1 = [];
-      const diagonal2 = [];
-      for (let i = 0; i < matrix.length; i++) {
-        const colonna = [];
-        for (let j = 0; j < matrix.length; j++) {
-          colonna.push(matrix[j][i]);
-        }
-        diagonal1.push(matrix[i][i]);
-        diagonal2.push(matrix[i][matrix.length - 1 - i]);
-        arrays.push(matrix[i], colonna)
+    const diagonal1 = [];
+    const diagonal2 = [];
+    for (let i = 0; i < matrix.length; i++) {
+      const colonna = [];
+      for (let j = 0; j < matrix.length; j++) {
+        colonna.push(matrix[j][i]);
       }
-      arrays.push(diagonal1, diagonal2)
-      for(let i = 0; i < arrays.length; i++){
-        if (arrays[i].find((s) => s != symbol) === undefined) {
-          vittoria();
-          return;
-        }
-      }
-
-      
-      let even = true;
-
-      for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-          if (matrix[j][i] === 0) {
-            even = false;
-            break;
-          }
-        }
-      }
-      if (even) {
-        pareggio()
+      diagonal1.push(matrix[i][i]);
+      diagonal2.push(matrix[i][matrix.length - 1 - i]);
+      arrays.push(matrix[i], colonna)
+    }
+    arrays.push(diagonal1, diagonal2)
+    for(let i = 0; i < arrays.length; i++){
+      if (arrays[i].find((s) => s != simboli[k]) === undefined) {
+        vittoria(simboli[k]);
         return;
       }
+    }
 
+    
+    let even = true;
+
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (matrix[j][i] === 0) {
+          even = false;
+          break;
+        }
+      }
+    }
+    if (even) {
+      pareggio()
+      return;
+    }
+    }
+    
+
+  },[matrix])
+
+
+  function handleClick(row, col) {
+    if (matrix[row][col] == 0){
       setMatrix(() => {
         const newMatrix = [...matrix];
         newMatrix[row][col] = symbol;
@@ -54,18 +63,18 @@ export function Tris() {
       setSymbol(symbol === 1 ? 2 : 1);
     }
   }
-  function vittoria() {
-    alert(
-      `il simbolo che ha vinto è ${
-        symbol == 1 ? "croce" : symbol == 2 ? "cerchio" : ""
-      }`
-    );
-    setMatrix([
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ]);
-    setSymbol(1);
+  function vittoria(vincente) {
+   setTimeout(()=>{alert(
+    `il simbolo che ha vinto è ${
+      vincente == 1 ? "croce" : vincente == 2 ? "cerchio" : ""
+    }`
+  );
+  setMatrix([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]);
+  setSymbol(1);},100) 
   }
 
   function pareggio() {
@@ -77,7 +86,7 @@ export function Tris() {
     ]);
     setSymbol(1);
   }
-
+  console.log("rendering")
   return (
     <div className="game">
       <div className="game-container">
