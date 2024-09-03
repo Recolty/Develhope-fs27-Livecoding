8d6749d8765a17450d44b6bbdd9e5bb5713425e5
Francesca_Francyprogrammer <francescadesimone850@gmail.com>
2024-09-03 11:03:08 +0200

    Creata funzione update

---
+++ React/live-30-07-2024/my-app/src/Scelta.jsx
@@ -0,0 +1,11 @@
+import { React } from "react";
+
+function Scelta() {
+    const [scelta, setScelta] = useState(1)
+}
+
+<div>
+    Ciao!
+</div>
+
+export default Scelta
--- React/live-30-07-2024/my-app/src/Tris.jsx
+++ React/live-30-07-2024/my-app/src/Tris.jsx
@@ -8,53 +8,48 @@ export function Tris() {
   ]);
   const [symbol, setSymbol] = useState(1);
 
-  useEffect(()=>{
-    console.log("controllo")
-    const simboli = [1,2]
-    for(let k = 0; k<simboli.length;k++){
-      const arrays = []
-    const diagonal1 = [];
-    const diagonal2 = [];
-    for (let i = 0; i < matrix.length; i++) {
-      const colonna = [];
-      for (let j = 0; j < matrix.length; j++) {
-        colonna.push(matrix[j][i]);
+  useEffect(() => {
+    const simboli = [1, 2];
+    for (let k = 0; k < simboli.length; k++) {
+      const arrays = [];
+      const diagonal1 = [];
+      const diagonal2 = [];
+      for (let i = 0; i < matrix.length; i++) {
+        const colonna = [];
+        for (let j = 0; j < matrix.length; j++) {
+          colonna.push(matrix[j][i]);
+        }
+        diagonal1.push(matrix[i][i]);
+        diagonal2.push(matrix[i][matrix.length - 1 - i]);
+        arrays.push(matrix[i], colonna);
       }
-      diagonal1.push(matrix[i][i]);
-      diagonal2.push(matrix[i][matrix.length - 1 - i]);
-      arrays.push(matrix[i], colonna)
-    }
-    arrays.push(diagonal1, diagonal2)
-    for(let i = 0; i < arrays.length; i++){
-      if (arrays[i].find((s) => s != simboli[k]) === undefined) {
-        vittoria(simboli[k]);
+      arrays.push(diagonal1, diagonal2);
+      for (let i = 0; i < arrays.length; i++) {
+        if (arrays[i].find((s) => s != simboli[k]) === undefined) {
+          vittoria(simboli[k]);
+          return;
+        }
+      }
+
+      let even = true;
+
+      for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+          if (matrix[i][j] === 0) {
+            even = false;
+            break;
+          }
+        }
+      }
+      if (even) {
+        pareggio();
         return;
       }
     }
-
-    
-    let even = true;
-
-    for (let i = 0; i < matrix.length; i++) {
-      for (let j = 0; j < matrix[i].length; j++) {
-        if (matrix[j][i] === 0) {
-          even = false;
-          break;
-        }
-      }
-    }
-    if (even) {
-      pareggio()
-      return;
-    }
-    }
-    
-
-  },[matrix])
-
+  }, [matrix]);
 
   function handleClick(row, col) {
-    if (matrix[row][col] == 0){
+    if (matrix[row][col] == 0) {
       setMatrix(() => {
         const newMatrix = [...matrix];
         newMatrix[row][col] = symbol;
@@ -63,30 +58,35 @@ export function Tris() {
       setSymbol(symbol === 1 ? 2 : 1);
     }
   }
+
   function vittoria(vincente) {
-   setTimeout(()=>{alert(
-    `il simbolo che ha vinto è ${
-      vincente == 1 ? "croce" : vincente == 2 ? "cerchio" : ""
-    }`
-  );
-  setMatrix([
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-  ]);
-  setSymbol(1);},100) 
+    setTimeout(() => {
+      alert(
+        `il simbolo che ha vinto Ã¨ ${
+          vincente == 1 ? "croce" : vincente == 2 ? "cerchio" : ""
+        }`
+      );
+      setMatrix([
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+      ]);
+      setSymbol(1);
+    }, 100);
   }
 
   function pareggio() {
-    alert(`pareggio`);
-    setMatrix([
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ]);
-    setSymbol(1);
+    setTimeout(() => {
+      alert(`pareggio`);
+      setMatrix([
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+      ]);
+      setSymbol(1);
+    }, 100);
   }
-  console.log("rendering")
+
   return (
     <div className="game">
       <div className="game-container">
@@ -95,17 +95,23 @@ export function Tris() {
             return (
               <div
                 className="check"
-                key={colonna}
+                key={riga + "" + colonna}
                 onClick={() => {
                   handleClick(riga, colonna);
                 }}
               >
-                {" "}
                 <div
                   className={`${
-                    col === 1 ? "line" : col === 2 ? "circle" : ""
+                    col === 1 ? "cross" : col === 2 ? "circle" : ""
                   }`}
-                ></div>{" "}
+                >
+                  {col === 1 && (
+                    <>
+                      <div className="line1"></div>
+                      <div className="line2"></div>
+                    </>
+                  )}
+                </div>
               </div>
             );
           })
